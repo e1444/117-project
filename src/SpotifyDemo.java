4478cb5d7493de8163add54f5d492e01e96f8e5d
Eric Liang <eliangca@outlook.com>
2023-09-27 14:16:24 -0400

    Added SpotifyDemo.requestArtistData and SpotifyDemo.requestTrackData methods

--- src/SpotifyDemo.java
+++ src/SpotifyDemo.java
@@ -9,8 +9,17 @@ public class SpotifyDemo {
     // load CLIENT_ID and CLIENT_SECRET from env variable.
     private static final String CLIENT_ID = System.getenv("CLIENT_ID");
     private static final String CLIENT_SECRET = System.getenv("CLIENT_SECRET");
+
     public static void main(String[] args) {
-        System.out.println(SpotifyDemo.requestAccessToken());
+        String accessToken = SpotifyDemo.requestAccessToken();
+        System.out.println(accessToken);
+
+        JSONObject artistData = SpotifyDemo.requestArtistData(accessToken, "3ICflSq6ZgYAIrm2CTkfVP");
+        System.out.println(artistData.toString(4));
+        // https://open.spotify.com/track/3CnUGF7phvydXWBJUBDDP6?si=15b4460d739f45c9
+        JSONObject trackData = SpotifyDemo.requestTrackData(accessToken, "3CnUGF7phvydXWBJUBDDP6");
+        System.out.println(trackData.toString(4));
+
     }
 
     public static String requestAccessToken() {
@@ -39,4 +48,41 @@ public class SpotifyDemo {
             throw new RuntimeException(e);
         }
     }
+
+    public static JSONObject requestArtistData(String accessToken, String artistURI) {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/artists/".concat(artistURI))
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static JSONObject requestTrackData(String accessToken, String trackURI) {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/tracks/".concat(trackURI))
+                .addHeader("Authorization", "Bearer " + accessToken)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return new JSONObject(response.body().string());
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
